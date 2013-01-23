6e8f049353223c85ba1b3485ad860ee314c46a39
cpeterson <cpeterson@gettyimages.com>
2013-01-23 13:45:21 -0800

    Add handling for cert validation errors (can occur when connecting to endpoint over HTTPS)

--- Driver/Discovery.cs
+++ Driver/Discovery.cs
@@ -3,8 +3,11 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
+using System.Net.Security;
+using System.Security.Cryptography.X509Certificates;
 using System.Web.Services.Description;
 using System.Web.Services.Discovery;
+using System.Windows;
 
 namespace Driver
 {
@@ -13,6 +16,12 @@ namespace Driver
 		readonly string uri;
 		readonly ICredentials credentials;
 		DiscoveryClientDocumentCollection documents;
+
+        // Handle SSL cert errors (see http://stackoverflow.com/questions/777607/the-remote-certificate-is-invalid-according-to-the-validation-procedure-ple)
+        static Discovery()
+        {
+            ServicePointManager.ServerCertificateValidationCallback = OnServerCertificateValidationCallback;
+        }
 
 		public Discovery(string uri, ICredentials credentials)
 		{
@@ -35,10 +44,33 @@ namespace Driver
 			return documents ?? (documents = DiscoverDocuments());
 		}
 
-		DiscoveryClientDocumentCollection DiscoverDocuments()
+	    private static bool OnServerCertificateValidationCallback(
+            object sender, X509Certificate certificate, X509Chain chain, SslPolicyErrors sslPolicyErrors)
+	    {
+	        if (sslPolicyErrors == SslPolicyErrors.None)
+	            return true;
+
+	        if ((sslPolicyErrors & SslPolicyErrors.RemoteCertificateChainErrors) > 0)
+	        {
+	            var certStatus = chain.ChainStatus.Last();
+	            if (MessageBox.Show(string
+	                                    .Format(
+	                                        "{0} (Code: {1}).{2}{2}Continue adding the connection?",
+	                                        certStatus.StatusInformation.TrimEnd().TrimEnd('.'), certStatus.Status, Environment.NewLine),
+	                                "Certificate Validation Error",
+	                                MessageBoxButton.YesNo, MessageBoxImage.Warning) == MessageBoxResult.Yes)
+	            {
+	                return true;
+	            }
+	        }
+	        return false;
+	    }
+
+	    DiscoveryClientDocumentCollection DiscoverDocuments()
 		{
 			var protocol = new DiscoveryClientProtocol {
 				AllowAutoRedirect = true,
+                
 				Credentials = credentials ?? CredentialCache.DefaultCredentials
 			};
 
