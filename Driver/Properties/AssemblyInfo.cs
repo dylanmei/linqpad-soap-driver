13cfb7580c5c4cc9ec25583a656b9cf2cc6fd8cb
dylanmei <dylanmei@gmail.com>
2013-09-21 08:43:53 -0700

    up the version

--- Driver/Properties/AssemblyInfo.cs
+++ Driver/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("SoapDriver")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("SoapContextDriver")]
+[assembly: AssemblyDescription("SOAP service driver for LINQPad")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("SoapDriver")]
-[assembly: AssemblyCopyright("")]
+[assembly: AssemblyProduct("SoapContextDriver")]
+[assembly: AssemblyCompany("Dylan Meissner")]
+[assembly: AssemblyCopyright("Copyright © 2011-2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.2.1")]
-[assembly: AssemblyFileVersion("1.2.1")]
+[assembly: AssemblyVersion("1.2.2")]
+[assembly: AssemblyFileVersion("1.2.2")]
